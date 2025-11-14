3f1a2aab858e99dcb0628eb701b69cda8e65b426
Samuel Zion <samzion899@gmail.com>
2025-11-14 13:45:34 +0000

    fixed_version

--- login_.py
+++ login_.py
@@ -1,39 +1,41 @@
 import bcrypt
 
-password = 'Password123'
-
 def hash_password(password):
     binary_password = password.encode('utf-8')
     salt = bcrypt.gensalt()
-    hash_password = bcrypt.hashpw(binary_password,salt)
-    return hash_password.decode('utf-8')
+    hashed = bcrypt.hashpw(binary_password, salt)
+    return hashed.decode('utf-8')
 
 
-def validate_password(password):
-    hash_password = hash.encode('utf-8')
+def validate_password(password, stored_hash):
     bin_password = password.encode('utf-8')
+    bin_stored = stored_hash.encode('utf-8')
 
-    return(bcrypt.checkpw(bin_password, hash_password))
+    return bcrypt.checkpw(bin_password, bin_stored)
+
 
 def register_user():
     name = input('enter a name: ').strip()
     pswrd = input('enter a password: ').strip()
-    hash = hash_password(pswrd)
+    hashed = hash_password(pswrd)
+
     with open('users.txt', 'a') as f:
-        f.write((f'{name},{hash}\n'))
-    print('user registered succesfully')
+        f.write(f'{name},{hashed}\n')
+
+    print('user registered successfully')
 
 
 def login_user():
-    name = input('enter a name').strip()
-    pswrd = input('enter a password').strip()
-    with open('users.txt','r') as f:
-        user = f.readlines()
-    
-    for user in user:
-        name_, hash = user.strip().split(',')
+    name = input('enter a name: ').strip()
+    pswrd = input('enter a password: ').strip()
 
-        if name_ == name:
-            return validate_hash(password, hash)
+    with open('users.txt', 'r') as f:
+        users = f.readlines()
+
+    for user in users:
+        stored_name, stored_hash = user.strip().split(',')
+
+        if stored_name == name:
+            return validate_password(pswrd, stored_hash)
+
     return False
-
